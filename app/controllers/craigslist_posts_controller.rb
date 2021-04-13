11cbd309d473d3563e89919f0228ee3c50664dc3
Your Name <you@example.com>
2021-04-12 22:17:22 -0500

    Makes Mark Seen use remote true

--- app/controllers/craigslist_posts_controller.rb
+++ app/controllers/craigslist_posts_controller.rb
@@ -2,23 +2,23 @@ class CraigslistPostsController < ApplicationController
   before_action :set_craigslist_post, only: %i(update destroy)
 
   def update
-    if @post.update(post_params)
-      flash[:notice] = 'Post updated'
-      redirect_to alert_path(@post.alert)
-    else
-      flash[:error] = 'Could not update post'
-      redirect_to alert_path(@post.alert)
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to(alert_path(@post.alert), notice: 'Post updated') }
+        format.js
+      else
+        format.html { redirect_to(alert_path(@post.alert), error: 'Could not update post') }
+      end
     end
   end
 
   def destroy
     respond_to do |format|
       if @post.destroy
-        format.html { redirect_to alert_path(@post.alert), notice: 'Post deleted.' }
+        format.html { redirect_to(alert_path(@post.alert), notice: 'Post deleted.') }
         format.js
       else
-        format.html { redirect_to alert_path(@post.alert), notice: 'Could not delete post.' }
-        format.js
+        format.html { redirect_to(alert_path(@post.alert), notice: 'Could not delete post.') }
       end
     end
   end
