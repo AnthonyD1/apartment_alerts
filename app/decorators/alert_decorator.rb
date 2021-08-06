6ac2d259a84a48a5036146d1a889c023cff4602c
Your Name <you@example.com>
2021-08-05 20:21:20 -0500

    Refactor: Tags to be human friendly

--- app/decorators/alert_decorator.rb
+++ app/decorators/alert_decorator.rb
@@ -1,7 +1,34 @@
 class AlertDecorator < ApplicationDecorator
   delegate_all
 
+  BOOLEAN_TAGS = HashWithIndifferentAccess.new({ hasPic: 'has pics', pets_cat: 'cat friendly', pets_dog: 'dog friendly', is_furnished: 'furnished', no_smoking: 'non smoking', wheelchaccess: 'wheelchair access', ev_charging: 'ev charging', application_fee: 'application fee', broker_fee: 'broker fee' })
+  COMPOUND_TAGS = %i[min_price max_price min_bedrooms max_bedrooms min_bathrooms max_bathrooms minSqft maxSqft]
+
   def search_param_tags
-    model.filtered_search_params.except(:postedToday)
+    tags = model.filtered_search_params.except(:postedToday)
+
+    regular_tags = tags.except(*COMPOUND_TAGS).map do |key, value|
+      if BOOLEAN_TAGS.keys.include?(key)
+        BOOLEAN_TAGS[key]
+      else
+        "#{key.to_s.humanize.downcase}: #{value}"
+      end
+    end
+
+    [format(tags, :price), format(tags, :bedrooms), format(tags, :bathrooms), format(tags, :Sqft), regular_tags].flatten.compact
+  end
+
+  private
+
+  def format(tags, key)
+    return if tags.keys.grep(/#{key}/).blank?
+
+    min_key = key == :Sqft ? "min#{key.to_s}" : "min_#{key.to_s}"
+    max_key = key == :Sqft ? "max#{key.to_s}" : "max_#{key.to_s}"
+    min_value = tags[min_key] || 0
+    max_value = tags[max_key] || 'infinity'
+
+    "#{key.to_s}: #{min_value} - #{max_value}"
   end
 end
+
--- spec/decorators/alert_decorator_spec.rb
+++ spec/decorators/alert_decorator_spec.rb
@@ -1,11 +1,52 @@
+require 'rails_helper'
+
 RSpec.describe AlertDecorator do
   describe '#search_param_tags' do
     it 'returns params excluding postedToday' do
-      alert = Alert.new(city: 'des moines', search_params: { hasPic: '1', postedToday: '1' })
+      search_params = HashWithIndifferentAccess.new({ hasPic: '1', postedToday: '1' })
+      alert = Alert.new(city: 'des moines', search_params: search_params)
 
       alert_decorator = alert.decorate
 
-      expect(alert_decorator.search_param_tags).to eq({ hasPic: '1' })
+      expect(alert_decorator.search_param_tags).to eq(['has pics'])
+    end
+
+    it 'formats boolean tags properly' do
+      search_params = HashWithIndifferentAccess.new({ pets_dog: '1' })
+      alert = Alert.new(city: 'des moines', search_params: search_params)
+
+      alert_decorator = alert.decorate
+
+      expect(alert_decorator.search_param_tags).to eq(['dog friendly'])
+    end
+
+    it 'formats regular tags properly' do
+      search_params = HashWithIndifferentAccess.new({ search_distance: '10' })
+      alert = Alert.new(city: 'des moines', search_params: search_params)
+
+      alert_decorator = alert.decorate
+
+      expect(alert_decorator.search_param_tags).to eq(['search distance: 10'])
+    end
+
+    context 'compound tags' do
+      it 'formats square feet correctly' do
+        search_params = HashWithIndifferentAccess.new({ minSqft: '100', maxSqft: '600' })
+        alert = Alert.new(city: 'des moines', search_params: search_params)
+
+        alert_decorator = alert.decorate
+
+        expect(alert_decorator.search_param_tags).to eq(['Sqft: 100 - 600'])
+      end
+
+      it 'formats price correctly' do
+        search_params = HashWithIndifferentAccess.new({ min_price: '100', max_price: '600' })
+        alert = Alert.new(city: 'des moines', search_params: search_params)
+
+        alert_decorator = alert.decorate
+
+        expect(alert_decorator.search_param_tags).to eq(['price: 100 - 600'])
+      end
     end
   end
 end
