018f259e68d2ac880f6dd6f7b01b07b239e093c0
Your Name <you@example.com>
2021-10-02 22:16:16 -0500

    Sets default_url_options for Production
    This is needed for mailers to generate the correct url for our app.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # Mailer Options
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: '<APPNAME>.heroku.com' }
+  config.action_mailer.default_url_options = { host: 'polar-waters-53263.herokuapp.com' }
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
