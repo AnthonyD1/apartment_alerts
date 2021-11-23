75eb144710f54a4dde0f66bdf8d91df1c9562966
Your Name <you@example.com>
2021-11-22 21:56:58 -0600

    Feat: Handles Redirect for TOR
    This will allow us to handle redirects when using TOR locally. The use
    case here would be when the zipcode for the Alert does not match the
    city. Craigslist will redirect you to the correct CL city listings based
    on the supplied zip.

--- app/modules/tor.rb
+++ app/modules/tor.rb
@@ -1,8 +1,24 @@
 require 'socksify/http'
 
 module Tor
+  class Http
+    def self.get(uri)
+      resp = get_request(uri)
+
+      resp = self.get_request(URI(resp['location'])) if resp.code == '301'
+
+      resp.body
+    end
+
+    def self.get_request(uri)
+      resp = Net::HTTP.SOCKSProxy('127.0.0.1', 9050).start(uri.host, uri.port, use_ssl: true) do |http|
+        http.get(uri)
+      end
+    end
+  end
+
   def http
-    @http ||= Net::HTTP::SOCKSProxy('127.0.0.1', 9050)
+    @http ||= Http
   end
 
   def generate_new_tor_circuit
